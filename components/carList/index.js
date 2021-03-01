9b817982af93410ff21dd207af34663ea9d0af02
Jack <jack.branthwaite@gmail.com>
2021-03-01 21:33:56 +1300

    Snapped scrolling/removal of scroll indicator

--- components/carList/index.js
+++ components/carList/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Dimensions } from "react-native";
 import styles from "./styles";
 import cars from "./cars";
 import CarItem from "../carItem";
@@ -8,7 +8,14 @@ import CarItem from "../carItem";
 const CarList = (props) => {
   return (
     <View style={styles.container}>
-      <FlatList data={cars} renderItem={({ item }) => <CarItem car={item} />} />
+      <FlatList
+        data={cars}
+        renderItem={({ item }) => <CarItem car={item} />}
+        snapToAlignment={"start"}
+        decelerationRate={"fast"}
+        snapToInterval={Dimensions.get("window").height}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
