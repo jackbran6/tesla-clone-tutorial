bc0d00972ab02b80c2a0b9c299320a0a8f731005
Jack <jack.branthwaite@gmail.com>
2021-03-01 20:58:00 +1300

    Removing hardcoded informaiton from caritem

--- components/StyleButton/index.js
+++ components/StyleButton/index.js
@@ -11,7 +11,7 @@ const StyledButton = (props) => {
   const { type, content, onPress } = props;
 
   const backgroundColor = type === "primary" ? "#171A20CC" : "#FFFFFFA6";
-  const textColor = type === "primary" ? "FFFFFF" : "171A20";
+  const textColor = type === "secondary" ? "#171A20CC" : "#FFFFFFA6";
 
   return (
     <View style={styles.container}>
